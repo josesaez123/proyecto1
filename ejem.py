ffec48123861e2238ece9d03847d87a25e0d054b
CarlosMirandaG <86996860+CarlosMirandaG@users.noreply.github.com>
2021-07-06 19:31:46 -0400

    Update ejem.py

--- ejem.py
+++ ejem.py
@@ -3,34 +3,28 @@ listac = ["15", "01", "02", "03", "04", "05", "13", "06", "07", "16", "08", "09"
 listar = ["Region de Arica y Parinacota", "Region de Tarapacá", "Region de Antofagasta", "Region de Atacama", "Region de Coquimbo", "Region de Valparaiso", "Region Metropolitana", "Region del Libertador General Bernardo O'higgins", "Region del Maule", "Region del Ñuble", "Region del Biobio", "Region de la Araucania", "Region de los Rios", "Region de los Lagos", "Region de Aysén del General Carlos Ibáñez del Campo", "Region de Magallanes"]
 x   =   0
 
-pepe = int(input("Ingrese una region o su codigo: "))
+mp = int(input("""
+------------Ingrese Una Region o su codigo------------
 
-for i in range(listar):
-    if pepe[0]  ==  listar[i]:
-        x = 5
-
-    if pepe[0]  ==  listac[i]:
-        x = 10
-
-print ("""
-15.Región de Arica y Parinacota
-01.Región de Tarapacá
+15.Region de Arica y Parinacota
+01.Region de Tarapacá
 02.Region de Antofagasta
 03.Region de Atacama
-04.Región de Coquimbo
-05.Región de Valparaiso
-13.Región Metropolitana
-06.Región del Libertador General Bernardo O'higgins
-07.Región del Maule
-16.Región del Ñuble
-08.Región del Biobio
-09.Región de la Araucania
-14.Región de los Rios
-10.Región de los Lagos
-11.Región de Aysén del General Carlos Ibáñez del Campo
-12.Región de Magallanes
-""")
+04.Region de Coquimbo
+05.Region de Valparaiso
+13.Region Metropolitana
+06.Region del Libertador General Bernardo O'higgins
+07.Region del Maule
+16.Region del Ñuble
+08.Region del Biobio
+09.Region de la Araucania
+14.Region de los Rios
+10.Region de los Lagos
+11.Region de Aysén del General Carlos Ibáñez del Campo
+12.Region de Magallanes
 
+------------------------------------------------------
+"""))
 if x == 1:
     print("a.- Examenes PCR acumulativos")
     print("b.- Examenes PCR no acumulativos")
