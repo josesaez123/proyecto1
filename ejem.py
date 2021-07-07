fcd2edd1955922811ef761ac2e12b7cbe8aae4cc
josesaez123 <86996839+josesaez123@users.noreply.github.com>
2021-07-07 02:18:15 -0400

    Update ejem.py

--- ejem.py
+++ ejem.py
@@ -67,21 +67,119 @@ while (mp > 0):
 ----------------------------------------""")
         z = input("<> ")
     
-        if z == "a":
+listac = ["15", "01", "02", "03", "04", "05", "13", "06", "07", "16", "08", "09", "14", "10", "11", "12"]
 
-            import matplotlib.pyplot as plt
-            ejex=[4,8,13,17,20]
-            ejey=[54,67,98,78,45]
-            plt.plot(ejex,ejey)
-            plt.show()
+listar = ["Region de Arica y Parinacota", "Region de Tarapacá", "Region de Antofagasta", "Region de Atacama", "Region de Coquimbo", "Region de Valparaiso", "Region Metropolitana", "Region del Libertador General Bernardo O'higgins", "Region del Maule", "Region del Ñuble", "Region del Biobio", "Region de la Araucania", "Region de los Rios", "Region de los Lagos", "Region de Aysén del General Carlos Ibáñez del Campo", "Region de Magallanes"]
+x   =   0
 
-        if z == "b":
+
+x = int(input("Ingrese una region o su codigo: "))
+
+if x == 15:
     
-            import matplotlib.pyplot as plt
-            ejex=[999,1000,13,17,20]
-            ejey=[54,67,98,78,45]
-            plt.plot(ejex,ejey)
-            plt.show()
+    z = input("""
+    a.- Examenes PCR acumulativos
+    b.- Examenes PCR no acumulativos)
+    Ingrese una opcion: """)
+
+    if z == "a":
+
+        import matplotlib.pyplot as plt
+        ejex=[0,"23","24","25","26","27","28","29","30","1","2","3","4","5","6"]
+        ejey=[0,757,848,1344,1210,1089,1669,824,660,689,1304,975,1042,1076,782]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+    if z == "b":
+    
+        import matplotlib.pyplot as plt
+        ejex=[0,"23-24","25-26","27-28","29-30","1-2","3-4","5-6"]
+        ejey=[0,1605,2554,2758,1484,1993,2017,1858]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+if x == 01:
+    print("a.- Examenes PCR acumulativos")
+    print("b.- Examenes PCR no acumulativos")
+    z = input("Ingrese una opcion: ")
+
+    if z == "a":
+
+        import matplotlib.pyplot as plt
+        ejex=[0,"23","24","25","26","27","28","29","30","1","2","3","4","5","6"]
+        ejey=[0,1229,1121,1368,1156,1468,2306,1498,917,1082,1285,1236,1348,2886,1003]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+    if z == "b":
+    
+        import matplotlib.pyplot as plt
+        ejex=[0,"23-24","25-26","27-28","29-30","1-2","3-4","5-6"]
+        ejey=[0,2350,2524,3774,2415,2367,2584,3889]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+if x == 02:
+    print("a.- Examenes PCR acumulativos")
+    print("b.- Examenes PCR no acumulativos")
+    z = input("Ingrese una opcion: ")
+
+    if z == "a":
+
+        import matplotlib.pyplot as plt
+        ejex=[0,"23","24","25","26","27","28","29","30","1","2","3","4","5","6"]
+        ejey=[0,913,2496,2505,2290,2440,4993,5152,907,2279,2642,2250,2712,2850,4430]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+    if z == "b":
+    
+        import matplotlib.pyplot as plt
+        ejex=[0,"23-24","25-26","27-28","29-30","1-2","3-4","5-6"]
+        ejey=[0,3409,4795,7433,6059,4921,4962,7280]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+if x == 03:
+    print("a.- Examenes PCR acumulativos")
+    print("b.- Examenes PCR no acumulativos")
+    z = input("Ingrese una opcion: ")
+
+    if z == "a":
+
+        import matplotlib.pyplot as plt
+        ejex=[0,"23","24","25","26","27","28","29","30","1","2","3","4","5","6"]
+        ejey=[0,622,752,1894,1497,1378,1405,1101,1535,729,781,2531,1636,1468,1026]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+    if z == "b":
+    
+        import matplotlib.pyplot as plt
+        ejex=[0,"23","24","25","26","27","28","29","30","1","2","3","4","5","6"]
+        ejey=[0,622,1374,3268,4765,6143,7548,8649,10184,10913,11694,14225,15861,17329,18355]
+        plt.plot(ejex,ejey)
+        plt.show()
+if x == 04:
+    print("a.- Examenes PCR acumulativos")
+    print("b.- Examenes PCR no acumulativos")
+    z = input("Ingrese una opcion: ")
+
+    if z == "a":
+
+        import matplotlib.pyplot as plt
+        ejex=[0,"23","24","25","26","27","28","29","30","1","2","3","4","5","6"]
+        ejey=[0,1155,988,2701,2605,2609,2963,1221,1420,1150,1695,2775,3695,1518,1855]
+        plt.plot(ejex,ejey)
+        plt.show()
+
+    if z == "b":
+    
+        import matplotlib.pyplot as plt
+        ejex=[0,"23-24","25-26","27-28","29-30","1-2","3-4","5-6"]
+        ejey=[0,1155,2143,4844,7449,10058,13021,14242,15662,16812,18507,21282,24977,26495,28350]
+        plt.plot(ejex,ejey)
+        plt.show()
  
 print("holiwis")
 
